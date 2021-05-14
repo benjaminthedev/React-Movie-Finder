6b481e1e772d62621eb9b4b7ad169e2027fa850b
Benjamin Dordoigne <ben.dordoigne@gmail.com>
2021-05-14 09:16:50 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import Movie from './components/Movie';
 function App() {
 
   const movies = ['title one', 'title two', 'title three'];
+  console.log('Process ENVV:')
+  console.log(process.env);
+  console.log('API KEY:')
+  console.log(process.env.API_KEY);
+  console.log('API POP')
+  console.log(process.env.NODE_ENV);
 
 
   return (
     <div className="App">
       <h1>movie App</h1>
+      <small>You are running this application in <b>{process.env.API_KEY}</b>c mode.</small>
 
     {movies.map(movie => (
       <Movie />
